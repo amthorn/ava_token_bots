1115c8f04c606f9b3b353e56d92e423e2a88470a
Ava Thorn <avthorn@cisco.com>
2018-05-07 16:12:46 -0400

    added votebot

--- bot.py
+++ bot.py
@@ -42,6 +42,7 @@ class Bot:
             '^.*#[rR][iI][pP].*$': self.rip,
             # '^.*[rR][oO][nN]\s[sS][wW][aA][nN][sS][oO][nN].*$': self.ron,
             '^.*[mM][aA][gG][iI][cC]\s[eE][iI][gG][hH][tT](\s)?[bB][aA][lL][lL].*$': self.magic_eight_ball,
+            '^.*[vV][oO][tT][eE][bB][oO][tT].*$': self.vote_bot,
         }
         self.trigger_word_appears = False
 
@@ -64,8 +65,8 @@ class Bot:
             if accept_message and ((dms and data.get('roomType') == 'direct') or data['roomId'] in approved_rooms):
                 message = message.replace(self.api.people.me().displayName, "", 1).strip()
 
-                if data['personId'] == blake:
-                    pun(data, message)
+                # if data['personId'] == blake:
+                #     pun(data, message)
 
                 for regex, func in self.triggers.items():
                     if re.search(regex, message):
@@ -185,3 +186,46 @@ class Bot:
             files=['rip.png'],
             roomId=data['roomId']
         )
+
+    def vote_bot(self, data, message):
+        me = self.api.people.me()
+
+        message_id = request['data']['id']
+        message = self.api.messages.get(message_id).text
+
+        if message.startswith("new vote"):
+            options = [i.strip() for i in message.replace("new vote", "", 1).strip().split(' ')]
+            votes = {}
+            for i in options:
+                votes[i] = []
+
+            json.dump(votes, open('votes.json', 'w'))
+            self.api.messages.create(
+                roomId=request['data']['roomId'],
+                markdown="Let the voting begin. Tag me with your case-sensitive vote!"
+            )
+        elif message == 'talley':
+            votes = json.load(open('votes.json', 'r'))
+            self.api.messages.create(roomId=request['data']['roomId'], markdown=self.as_markdown(votes))
+        else:
+            votes = json.load(open('votes.json', 'r'))
+            if message in votes:
+                votes[message].append(self.api.people.get(request['data']['personId']).displayName)
+                votes[message] = list(set(votes[message]))
+                json.dump(votes, open('votes.json', 'w'))
+                self.api.messages.create(roomId=request['data']['roomId'], markdown=self.as_markdown(votes))
+            else:
+                self.api.messages.create(
+                    roomId=request['data']['roomId'],
+                    markdown="'" + str(message) + "' not a valid option, please use one of: " + ', '.join(list(votes.keys()))
+                )
+
+        return 'OK'
+
+    def as_markdown(self, votes):
+        message = ''
+        for k, v in votes.items():
+            message += str(k) + ': ' + str(len(v)) + ((' (' + ', '.join(v) + ')\n\n') if len(v) else '\n\n')
+        return message
+
+
